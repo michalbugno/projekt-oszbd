eaab3da41a99a6d18c22c9f5ca3d3a90911adf40
Michal Bugno <michal@bugno.net>
2009-02-17 19:59:37 +0100

    lotsa haxing

--- weather/settings.py
+++ weather/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('msq', 'michal.bugno@gmail.com'),
 )
 
 MANAGERS = ADMINS
--- weather/urls.py
+++ weather/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
+from django.contrib.gis import admin
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -14,4 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
